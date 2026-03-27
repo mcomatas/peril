ca8c7461fb85e20ac53605f61bba90a05305ba79
Michael Comatas <mcomatas99@gmail.com>
2026-03-27 13:53:52 -0400

    Updated server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connection := fmt.Sprintf("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial(connection)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to RabbitMQ")
+
+	// Wait for Ctrl+C
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("\nShutting down...")
 }
